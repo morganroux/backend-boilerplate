28450fd743a8bf4aae139946e909cc233f9a6ef5
Morgan Roux <roux.morgan@gmail.com>
2022-11-03 12:52:37 +0100

    fix: solve watching restart

--- src/api/rest/index.ts
+++ src/api/rest/index.ts
@@ -11,7 +11,7 @@ router.use("/hello", async (req, res) => {
   const test = await getTest("test");
   // eslint-disable-next-line no-console
   console.log("===", test);
-  res.status(200).send("Hello world !");
+  res.status(200).send("Hello world!");
 });
 
 export default router;
